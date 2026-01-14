4e54344a9f081864a0312f0f8a523547d22cf5ba
JEONG HYEON OH <52736095+zzocojoa@users.noreply.github.com>
2026-01-14 09:26:57 +0900

    feat(frontend): implement React Router with Home and Dashboard routes

--- v2_next/frontend/src/index.tsx
+++ v2_next/frontend/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { initScenesRuntime } from './scenes/ScenesRuntime';
 import { GlobalModalProvider } from './GlobalModalContext';
 import { CustomDialog } from './components/CustomDialog';
@@ -21,7 +23,12 @@ root.render(
   <React.StrictMode>
     <GlobalModalProvider>
       <ThemeProvider>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<App />} />
+          </Routes>
+        </BrowserRouter>
         <CustomDialog />
       </ThemeProvider>
     </GlobalModalProvider>
